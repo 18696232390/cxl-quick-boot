07df3c5aae9d9ec8335587d9f9ceb254475257de
Chen.js <18696232390@163.com>
2021-10-26 23:14:04 +0800

    init:初始化项目

---
+++ cxl-quick-api/src/main/java/com/cxl/quick/api/CxlQuickApiApplication.java
@@ -0,0 +1,13 @@
+package com.cxl.quick.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxlQuickApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CxlQuickApiApplication.class, args);
+    }
+
+}
---
+++ cxl-quick-api/src/test/java/com/cxl/quick/api/CxlQuickApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxl.quick.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxlQuickApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

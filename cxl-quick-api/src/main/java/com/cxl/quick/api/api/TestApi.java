1b3573426b5d95cbd6c58770889bb08e4e97a02f
Chen.js <18696232390@163.com>
2021-10-29 13:45:14 +0800

    Update TestApi.java

--- cxl-quick-api/src/main/java/com/cxl/quick/api/api/TestApi.java
+++ cxl-quick-api/src/main/java/com/cxl/quick/api/api/TestApi.java
@@ -1,5 +1,8 @@
 package com.cxl.quick.api.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName TestApi
  * @Description TODO
@@ -7,5 +10,10 @@ package com.cxl.quick.api.api;
  * @Date 2021/10/27 12:03 上午
  * @Version 1.0.0
  **/
+@RestController
+@RequestMapping("/test")
 public class TestApi {
+
+    
+
 }
